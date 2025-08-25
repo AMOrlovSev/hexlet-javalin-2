9bf4a1f15171edfc01b7db192107fabe05d81803
Artem <amorlov.sev@gmail.com>
2025-08-25 15:46:12 +0300

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    id("java")
+    id("application")
+}
+
+application {
+    mainClass = "org.example.hexlet.App"
 }
 
 group = "org.example"
@@ -10,8 +14,20 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    implementation("com.h2database:h2:2.3.232")
+    implementation("com.zaxxer:HikariCP:6.3.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    implementation("org.apache.commons:commons-text:1.13.1")
+    implementation("gg.jte:jte:3.2.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("io.javalin:javalin:6.6.0")
+    implementation("io.javalin:javalin-bundle:6.6.0")
+    implementation("io.javalin:javalin-rendering:6.6.0")
+
+    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
