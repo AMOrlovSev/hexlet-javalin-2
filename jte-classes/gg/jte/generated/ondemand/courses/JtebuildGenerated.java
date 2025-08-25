3876e8e7f91b0f2fd1528d7f52f25e08d28a7d41
Artem <amorlov.sev@gmail.com>
2025-08-25 19:13:13 +0300

    Модифицирующие формы

---
+++ jte-classes/gg/jte/generated/ondemand/courses/JtebuildGenerated.java
@@ -0,0 +1,16 @@
+package gg.jte.generated.ondemand.courses;
+@SuppressWarnings("unchecked")
+public final class JtebuildGenerated {
+	public static final String JTE_NAME = "courses/build.jte";
+	public static final int[] JTE_LINE_INFO = {0,0,0,0,0,0,2,2,18,18,18,18,18,18,18,18};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\r\n    <form action=\"/courses\" method=\"post\">\r\n        <div>\r\n            <label>\r\n                Название\r\n                <input type=\"text\" required name=\"name\" />\r\n            </label>\r\n        </div>\r\n        <div>\r\n            <label>\r\n                Описание\r\n                <input type=\"text\" name=\"description\" />\r\n            </label>\r\n        </div>\r\n        <input type=\"submit\" value=\"Добавить\" />\r\n    </form>\r\n");
+			}
+		}, null);
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}
